9a859b86335b5ba7f0ad7a79fb3f3196f9f00b64
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-14 22:59:44 -0500

    Included recursive linear search. Moved tests out.

--- chapter_2.py
+++ chapter_2.py
@@ -1,22 +1,8 @@
 # coding=utf-8
 # Search procedures described in chapter 2 of Cormen, T. H. (2013). Algorithms Unlocked. MIT Press. 
-# Linear search procedure described in pg. 13. Better linear search, described in page 14. sentinel linear search in pg. 16.
+# Linear search procedure described in pg. 13. Better linear search, described in page 14. sentinel linear search in pg. 16. Recursive linear search in pg. 24. 
 # Recursive factorial is described in pg. 23
 
-def run_search(tup, fun):
-    """Tests all search functions, fun, with the list, item and correct answer from tup. Returns a tuple containing tuples of function name, item, result.
-    tup is a tuple containing a list, an item to find in said list and the expected correct output.
-    fun is a list of search functions to test.
-    """
-    res=()
-    for t in tup:
-        for f in fun:
-            if f(t[0], t[1]) == t[2]:
-                res += (f, t[1], 'ok')
-            else:
-                res += (f, t[1], 'problemas')
-    return res
-    
 
 def linear_search(l, item):
     """ Returns True if item is in the list l, False if it is not."""
@@ -28,7 +14,7 @@ def linear_search(l, item):
 
 
 def better_linear_search(l, item):
-    """ Returns True if item is in the list l, False if it is not."""
+    """ Returns True if item is in the list l, False if it is not. Improves from linear_search by not checking until the end of the list if the item is found."""
     for i in l:
         if i==item:
             return True
@@ -57,26 +43,23 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 
-nfac=-1
+
+def bad_factorial(n):
+    """Will throw a maximum recursion depth error.
+    Calculates the factorial of int(n)>0 using a recursive algorithm that does not work properly because it can never reach the stop condition."""
+    if n<0:
+           raise ValueError("El número debe ser mayor que cero.")
+    if n == 0:
+        return 1
+    return (bad_factorial(n+1)/n+1)
 
 
-#l=['hola', 'no', "mañana", 'Clemencia', 'GLM', 1, 10, 245, 'glm']
-#item='no joda maldita sea'
-#print("Using linear search for '{0}' in {1}... {2}.".format(item, l, linear_search(l, item)))
-#print("Using better linear search for '{0}' in {1}... {2}.".format(item, l, better_linear_search(l, item)))
-#print("Using sentinel linear search for '{0}' in {1}... {2}.".format(item, l, sentinel_linear_search(l, item)))
-#print("Calculando {0}!... = {1}".format(nfac, factorial(nfac)))
-
-
-tup = (
-    (['hola', 'no', 'libro', 'ojo', 'good', 'bar'], 'bar', True),
-    ([1, 34, 54, 1012, 3011203040, 43, 0, -1], 1, True),
-    ([2, 4, 6, 8, 10], 11, False),
-    (range(10), 9, True),
-    (range(10**3), 9873, True),
-    (range(10**3), '4', False),
-)
-
-fun = [linear_search, better_linear_search, sentinel_linear_search]
-
-print(run_search(tup, fun))
+def recursive_linear_search(l, item):
+    """Returns True if item is in list(l) 
+    """
+    if len(l)==0:
+        return False
+    elif l[0]==item:
+        return True
+    else:
+        return recursive_linear_search(l[1:], item)
