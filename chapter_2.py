cce57f496fdb05bbbe699663908a4c56fbc47c98
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-14 20:25:46 -0500

    Fixed sentinel function and added an automated test for the search functions.

--- chapter_2.py
+++ chapter_2.py
@@ -1,7 +1,22 @@
 # coding=utf-8
 # Search procedures described in chapter 2 of Cormen, T. H. (2013). Algorithms Unlocked. MIT Press. 
 # Linear search procedure described in pg. 13. Better linear search, described in page 14. sentinel linear search in pg. 16.
+# Recursive factorial is described in pg. 23
 
+def run_search(tup, fun):
+    """Tests all search functions, fun, with the list, item and correct answer from tup. Returns a tuple containing tuples of function name, item, result.
+    tup is a tuple containing a list, an item to find in said list and the expected correct output.
+    fun is a list of search functions to test.
+    """
+    res=()
+    for t in tup:
+        for f in fun:
+            if f(t[0], t[1]) == t[2]:
+                res += (f, t[1], 'ok')
+            else:
+                res += (f, t[1], 'problemas')
+    return res
+    
 
 def linear_search(l, item):
     """ Returns True if item is in the list l, False if it is not."""
@@ -28,7 +43,7 @@ def sentinel_linear_search(l, item):
     while l[counter] != item:
         counter+=1
     l[-1]=last
-    if (counter < len(l)) or (l[-1]==item):
+    if counter < len(l)-1 or l[-1]==item:
         return True
     else:
         return False
@@ -45,9 +60,23 @@ def factorial(n):
 nfac=-1
 
 
-l=['hola', 'no', "mañana", 'Clemencia', 'GLM', 1, 10, 245, 'glm']
-item='GLM'
-print("Using linear search for '{0}' in {1}... {2}.".format(item, l, linear_search(l, item)))
-print("Using better linear search for '{0}' in {1}... {2}.".format(item, l, better_linear_search(l, item)))
-print("Using sentinel linear search for '{0}' in {1}... {2}.".format(item, l, better_linear_search(l, item)))
-print("Calculando {0}!... = {1}".format(nfac, factorial(nfac)))
+#l=['hola', 'no', "mañana", 'Clemencia', 'GLM', 1, 10, 245, 'glm']
+#item='no joda maldita sea'
+#print("Using linear search for '{0}' in {1}... {2}.".format(item, l, linear_search(l, item)))
+#print("Using better linear search for '{0}' in {1}... {2}.".format(item, l, better_linear_search(l, item)))
+#print("Using sentinel linear search for '{0}' in {1}... {2}.".format(item, l, sentinel_linear_search(l, item)))
+#print("Calculando {0}!... = {1}".format(nfac, factorial(nfac)))
+
+
+tup = (
+    (['hola', 'no', 'libro', 'ojo', 'good', 'bar'], 'bar', True),
+    ([1, 34, 54, 1012, 3011203040, 43, 0, -1], 1, True),
+    ([2, 4, 6, 8, 10], 11, False),
+    (range(10), 9, True),
+    (range(10**3), 9873, True),
+    (range(10**3), '4', False),
+)
+
+fun = [linear_search, better_linear_search, sentinel_linear_search]
+
+print(run_search(tup, fun))
