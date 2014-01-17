b779fd18f9f47353cb7409624c1d3ad5a37b54ba
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-17 15:24:38 -0500

    Docstring and comment for binary search

--- chapter_3.py
+++ chapter_3.py
@@ -2,6 +2,10 @@
 # My versions of procedures described in chapter 3 of Cormen, T. H. (2013). Algorithms Unlocked. MIT Press.
 # Version 0.1
 
+#Cormen defines de length of the list as an input. It is implied here
+#and extracted using the len function.
 def binary_search(l, item):
-     """ Returns True if item is in the list l, False if it is not."""
+     """ Returns index of item if item is in the list l,
+     False if it is not."""
      
+    
