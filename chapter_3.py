1cf6a1764e461ac1c4ec233f6276bc3352f7f8ad
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-17 15:18:37 -0500

    One line for binary search

--- chapter_3.py
+++ chapter_3.py
@@ -1,3 +1,7 @@
 # coding=utf-8
 # My versions of procedures described in chapter 3 of Cormen, T. H. (2013). Algorithms Unlocked. MIT Press.
 # Version 0.1
+
+def binary_search(l, item):
+     """ Returns True if item is in the list l, False if it is not."""
+     
