9595a68f00d96f609074c0dc7f26af9109fecd4d
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-20 12:11:56 -0500

    Added binary search

--- chapter_3.py
+++ chapter_3.py
@@ -1,11 +1,22 @@
 # coding=utf-8
-# My versions of procedures described in chapter 3 of Cormen, T. H. (2013). Algorithms Unlocked. MIT Press.
+# My versions of procedures described in chapter 3 of Cormen, T. H.
+#(2013). Algorithms Unlocked. MIT Press.
 # Version 0.1
 
-#Cormen defines de length of the list as an input. It is implied here
-#and extracted using the len function.
-def binary_search(l, item):
-     """ Returns index of item if item is in the list l,
-     False if it is not."""
-     
-    
+#Binary search described in page 30
+def binary_search(l, n, item):
+    """ Returns index of item if item is in the list l, False if it is
+    not. l is assumed to be ordered from least to greatest. n is and
+    int, the length of l"""
+    p=0
+    r=n-1
+    q=1
+    while p<=r:
+        q=(p+r)/2
+        if l[q]==item:
+            return q
+        elif l[q]>item:
+            r=q-1
+        elif l[q]<item:
+            p=q+1
+    return False
